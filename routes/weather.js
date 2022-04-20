376738a04c9c27f747c382df101ef05016134584
Emre Çolak <colakkemre@gmail.com>
2022-04-21 00:45:34 +0300

    cache api completed

--- routes/weather.js
+++ routes/weather.js
@@ -1,12 +1,16 @@
 // Import Packages
 const express = require('express');
-
+const apicache = require('apicache')
 // Import Controller
 const WeatherController = require('../controllers/weatherController');
+
+// Init cache
+let cache = apicache.middleware
+
 
 // Create express router
 const router = express.Router();
 
-router.route('/').get(WeatherController.requestFromServer);
+router.route('/').get(cache('2 minutes'),WeatherController.requestFromServer);
 
 module.exports = router
