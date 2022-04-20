448329869eb04db7b0ad8fef423c197d931de575
Emre Çolak <colakkemre@gmail.com>
2022-04-21 00:36:21 +0300

    request rate limiting completed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Import Packages
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
 
 // Import Router
 const WeatherRouter = require('./routes/weather');
@@ -9,9 +10,16 @@ const WeatherRouter = require('./routes/weather');
 // Create Express App
 const app = express();
 
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minitus
+  max: 100,
+})
+
 // Middlewares
 app.use(cors());
-
+app.use(limiter);
+app.set('trust proxy', 1);
 // Declare Port Number
 const PORT = 3000 
 
--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,5 +1,6 @@
 // Import Packages
 const needle = require('needle');
+const url = require('url');
 
 // Env variables
 const API_BASE_URL = process.env.API_BASE_URL;
@@ -10,11 +11,18 @@ exports.requestFromServer = async (req, res) => {
   try {
 
     const params = new URLSearchParams({
-      [API_KEY_NAME]:API_KEY_VALUE
+      [API_KEY_NAME]:API_KEY_VALUE,
+      ...url.parse(req.url, true).query
     })
 
     const apiRes = await needle('get',`${API_BASE_URL}?${params}`);
     const data = apiRes.body;
+
+    // log the request to public API
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`${API_BASE_URL}?${params}`);
+    }
+
     res.status(200).json(data);
   } catch(error) {
     res.status(400).json(error)
